be2819d4b4a8ebaf0c866345b6e5a8fbdeb6abe9
everzet <ever.zet@gmail.com>
2014-08-24 17:14:07 +0100

    Add InMemoryDocumentationProvider::registerDocumentation()

--- spec/Behat/Borg/Documentation/InMemory/InMemoryDocumentationProviderSpec.php
+++ spec/Behat/Borg/Documentation/InMemory/InMemoryDocumentationProviderSpec.php
@@ -2,14 +2,20 @@
 
 namespace spec\Behat\Borg\Documentation\InMemory;
 
-use Behat\Borg\Documentation\DocumentationProvider;
+use Behat\Borg\Documentation\Documentation;
+use Behat\Borg\Documentation\DocumentationId;
+use Behat\Borg\Documentation\DocumentationSource;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class InMemoryDocumentationProviderSpec extends ObjectBehavior
 {
-    function it_is_documentation_provider()
-    {
-        $this->shouldHaveType(DocumentationProvider::class);
+    function it_allows_dynamic_registration_of_documentation(
+        DocumentationId $anId,
+        DocumentationSource $source
+    ) {
+        $documentation = new Documentation($anId->getWrappedObject(), $source->getWrappedObject());
+
+        $this->registerDocumentation($documentation);
     }
 }
--- src/Behat/Borg/Documentation/InMemory/InMemoryDocumentationProvider.php
+++ src/Behat/Borg/Documentation/InMemory/InMemoryDocumentationProvider.php
@@ -2,8 +2,13 @@
 
 namespace Behat\Borg\Documentation\InMemory;
 
+use Behat\Borg\Documentation\Documentation;
 use Behat\Borg\Documentation\DocumentationProvider;
 
 final class InMemoryDocumentationProvider implements DocumentationProvider
 {
+    public function registerDocumentation(Documentation $documentation)
+    {
+        // TODO: write logic here
+    }
 }
