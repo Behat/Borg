dc2304c0e82e42080c2dd3a5ee2020ceb71cb94f
everzet <ever.zet@gmail.com>
2014-08-25 14:48:55 +0100

    Write an integration tests for SphinxDocumentationBuilder

--- tests/SphinxDocumentationBuilderTest.php
+++ tests/SphinxDocumentationBuilderTest.php
@@ -1,13 +1,105 @@
 <?php
 
+use Behat\Borg\Documentation\Documentation;
+use Behat\Borg\Documentation\DocumentationId;
+use Behat\Borg\Documentation\DocumentationSource;
+use Behat\Borg\SphinxDoc\Documentation\RstDocumentationSource;
 use Behat\Borg\SphinxDoc\DocumentationBuilder\SphinxDocumentationBuilder;
+use Symfony\Component\Filesystem\Filesystem;
 
 class SphinxDocumentationBuilderTest extends PHPUnit_Framework_TestCase
 {
+    private $tempInputPath;
+    private $tempOutputPath;
     private $builder;
 
     protected function setUp()
     {
-        $this->builder = new SphinxDocumentationBuilder();
+        $this->tempInputPath = __DIR__ . '/../test_temp/phpunit_input';
+        $this->tempOutputPath = __DIR__ . '/../test_temp/phpunit_output';
+
+        $this->builder = new SphinxDocumentationBuilder($this->tempOutputPath);
+
+        (new Filesystem())->remove([$this->tempInputPath, $this->tempOutputPath]);
+    }
+
+    /** @test */
+    function it_does_not_build_non_RST_documentation()
+    {
+        $anId = $this->createDocumentationId('myDoc');
+        $source = $this->createDocumentationSource();
+        $documentation = new Documentation($anId, $source);
+
+        $builtDocumentation = $this->builder->build($documentation);
+
+        $this->assertNull($builtDocumentation);
+    }
+
+    /** @test */
+    function it_builds_RST_documentation_into_the_output_path()
+    {
+        $anId = $this->createDocumentationId('myDoc');
+        $source = $this->createRstDocumentationSourceWithIndex("Docs\n====");
+        $documentation = new Documentation($anId, $source);
+
+        $built = $this->builder->build($documentation);
+
+        $this->assertFileExists($this->tempOutputPath . '/myDoc/index.html');
+        $this->assertEquals($this->tempOutputPath . '/myDoc/index.html', $built->getIndexPath());
+        $this->assertContains('<h1>Docs', file_get_contents($built->getIndexPath()));
+    }
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     */
+    function it_throws_an_exception_if_sphinx_can_not_build_documents()
+    {
+        $anId = $this->createDocumentationId('myDoc');
+        $source = $this->createRstDocumentationSourceWithoutIndex();
+        $documentation = new Documentation($anId, $source);
+
+        $this->builder->build($documentation);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return DocumentationId
+     */
+    private function createDocumentationId($id)
+    {
+        $anId = $this->getMock(DocumentationId::class);
+        $anId->method('__toString')->willReturn($id);
+
+        return $anId;
+    }
+
+    /**
+     * @return DocumentationSource
+     */
+    private function createDocumentationSource()
+    {
+        return $this->getMock(DocumentationSource::class);
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return RstDocumentationSource
+     */
+    private function createRstDocumentationSourceWithIndex($content)
+    {
+        (new Filesystem())->dumpFile($this->tempInputPath . '/index.rst', $content);
+
+        return RstDocumentationSource::atPath($this->tempInputPath);
+    }
+
+    /**
+     * @return RstDocumentationSource
+     */
+    private function createRstDocumentationSourceWithoutIndex()
+    {
+        return RstDocumentationSource::atPath($this->tempInputPath);
     }
 }
