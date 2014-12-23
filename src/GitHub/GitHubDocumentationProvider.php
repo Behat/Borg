1a9937a158233cef76b9b1224ee893bcf2e7e244
everzet <ever.zet@gmail.com>
2014-12-23 18:01:58 +0000

    Introduce GitHubDocumentationProvider

---
+++ src/GitHub/GitHubDocumentationProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Behat\Borg\GitHub;
+
+use Behat\Borg\Documentation\DocumentationId;
+use Behat\Borg\Documentation\DocumentationProvider;
+
+/**
+ * Provides documentation source using GitHub.
+ */
+final class GitHubDocumentationProvider implements DocumentationProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function findDocumentationById(DocumentationId $anId)
+    {
+        // TODO: Implement findDocumentationById() method.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAllDocumentation()
+    {
+        // TODO: Implement getAllDocumentation() method.
+    }
+}
