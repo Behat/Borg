6147fbc171fa5217ae93088a675215e18060adb2
everzet <ever.zet@gmail.com>
2014-12-23 20:45:34 +0000

    Implement GenerateDocumentationCommand

--- src/Application/DocumentationBundle/Command/GenerateDocumentationCommand.php
+++ src/Application/DocumentationBundle/Command/GenerateDocumentationCommand.php
@@ -2,12 +2,22 @@
 
 namespace Behat\Borg\Application\DocumentationBundle\Command;
 
+use Behat\Borg\DocumentationManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class GenerateDocumentationCommand extends Command
 {
+    private $manager;
+
+    public function __construct(DocumentationManager $manager)
+    {
+        $this->manager = $manager;
+
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
@@ -17,5 +27,6 @@ final class GenerateDocumentationCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->manager->buildDocumentation();
     }
 }
