580b3943b2d8a6b5a9d14fb906b7d363a821cb56
everzet <ever.zet@gmail.com>
2014-12-26 23:09:15 +0000

    Make ReleaseManager to submit events to listeners

--- spec/ReleaseManagerSpec.php
+++ spec/ReleaseManagerSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Behat\Borg;
 
+use Behat\Borg\Package\Listener\ReleaseListener;
 use Behat\Borg\Package\Package;
 use Behat\Borg\Package\Release;
 use Behat\Borg\Package\Version;
@@ -10,9 +11,19 @@ use Prophecy\Argument;
 
 class ReleaseManagerSpec extends ObjectBehavior
 {
-    function it_can_release_new_package_releases(Package $package)
-    {
+    function it_notifies_registered_listeners_during_release(
+        ReleaseListener $listener1,
+        ReleaseListener $listener2,
+        Package $package
+    ) {
         $aRelease = new Release($package->getWrappedObject(), Version::string('v2.5'));
+
+        $listener1->packageWasReleased($aRelease)->shouldBeCalled();
+        $listener2->packageWasReleased($aRelease)->shouldBeCalled();
+
+        $this->registerListener($listener1);
+        $this->registerListener($listener2);
+
         $this->release($aRelease);
     }
 }
--- src/ReleaseManager.php
+++ src/ReleaseManager.php
@@ -2,12 +2,22 @@
 
 namespace Behat\Borg;
 
+use Behat\Borg\Package\Listener\ReleaseListener;
 use Behat\Borg\Package\Release;
 
 final class ReleaseManager
 {
+    private $listeners = [];
+
+    public function registerListener(ReleaseListener $listener)
+    {
+        $this->listeners[] = $listener;
+    }
+
     public function release(Release $release)
     {
-        // TODO: write logic here
+        foreach ($this->listeners as $listener) {
+            $listener->packageWasReleased($release);
+        }
     }
 }
