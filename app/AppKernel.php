5ca7ef5db4754b39d98317806b1c217439e93f7d
everzet <ever.zet@gmail.com>
2014-12-27 19:38:50 +0000

    Generate DocumentationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Behat\Borg\Application\PackageBundle\PackageBundle(),
+            new Behat\Borg\Application\DocumentationBundle\DocumentationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
