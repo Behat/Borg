b0000e445b1d03caeeefb0c0dfcec1b8afa4762b
everzet <ever.zet@gmail.com>
2014-12-27 00:57:36 +0000

    Add missing doc blocks to ReleaseManager

--- src/ReleaseManager.php
+++ src/ReleaseManager.php
@@ -5,15 +5,31 @@ namespace Behat\Borg;
 use Behat\Borg\Package\Listener\ReleaseListener;
 use Behat\Borg\Package\Release;
 
+/**
+ * Manages package releases.
+ */
 final class ReleaseManager
 {
+    /**
+     * @var ReleaseListener[]
+     */
     private $listeners = [];
 
+    /**
+     * Registers new listener.
+     *
+     * @param ReleaseListener $listener
+     */
     public function registerListener(ReleaseListener $listener)
     {
         $this->listeners[] = $listener;
     }
 
+    /**
+     * Release provided package release.
+     *
+     * @param Release $release
+     */
     public function release(Release $release)
     {
         foreach ($this->listeners as $listener) {
