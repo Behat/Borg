955c8a1ef7b6a8a1c871d91054bbb83eb729191b
Christophe Coevoet <stof@notk.org>
2015-01-04 19:17:39 +0100

    Fix the implementation of the special Twig loader
    - throw a Twig_Error_Loader when calling getSource() on a template which
      is not found. This is the expected behavior of a Twig loader. The
      previous implementation was triggering a notice for non-matching names
      and trying to get the content of a non existent file for non-existent
      paths (which triggers a warning).
    - update the exists() implementation to update the internal cache for
      matched names to avoid a second file_exists check later when getting
      the source due to the fixed implementation.
    - remove the unused dependency on the Publisher.

--- src/Application/DocumentationBundle/Twig/DocumentationLoader.php
+++ src/Application/DocumentationBundle/Twig/DocumentationLoader.php
@@ -2,28 +2,34 @@
 
 namespace Behat\Borg\Application\DocumentationBundle\Twig;
 
-use Behat\Borg\Documentation\Publisher\Publisher;
+use Twig_Error_Loader;
 use Twig_ExistsLoaderInterface;
 use Twig_LoaderInterface;
 
 final class DocumentationLoader implements Twig_LoaderInterface, Twig_ExistsLoaderInterface
 {
     private static $regex = '#^documentation:(?<path>.*+)$#';
-    private $publisher;
     private $cache = [];
-
-    public function __construct(Publisher $publisher)
-    {
-        $this->publisher = $publisher;
-    }
 
     public function exists($template)
     {
+        $logicalName = (string) $template;
+
+        if (isset($this->cache[$logicalName])) {
+            return true;
+        }
+
         if (!preg_match(self::$regex, $template, $m)) {
             return false;
         }
 
-        return file_exists($m['path']);
+        if (file_exists($m['path'])) {
+            $this->cache[$logicalName] = $m['path'];
+
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -52,13 +58,19 @@ final class DocumentationLoader implements Twig_LoaderInterface, Twig_ExistsLoad
 
     private function findTemplate($template)
     {
-        $logicalName = (string)$template;
+        $logicalName = (string) $template;
 
         if (isset($this->cache[$logicalName])) {
             return $this->cache[$logicalName];
         }
 
-        preg_match(self::$regex, $template, $m);
+        if (!preg_match(self::$regex, $template, $m)) {
+            throw new Twig_Error_Loader(sprintf('Unsupported template name "%s" (expecting "documentation:path").', $logicalName));
+        }
+
+        if (!file_exists($m['path'])) {
+            throw new Twig_Error_Loader(sprintf('Unable to find template "%s"', $logicalName));
+        }
 
         return $this->cache[$logicalName] = $m['path'];
     }
