4e943ed4c7d53947b2321be508bc57723f30421d
everzet <ever.zet@gmail.com>
2015-01-11 20:42:35 +0000

    Switch FakeRepository to use InMemoryRepository

--- tests/Fake/Documentation/FakeRepository.php
+++ tests/Fake/Documentation/FakeRepository.php
@@ -5,29 +5,40 @@ namespace tests\Behat\Borg\Fake\Documentation;
 use Behat\Borg\Documentation\DocumentationId;
 use Behat\Borg\Documentation\Publisher\PublishedDocumentation;
 use Behat\Borg\Documentation\Repository\Repository;
+use Everzet\PersistedObjects\InMemoryRepository;
+use Everzet\PersistedObjects\ObjectIdentifier;
 
-final class FakeRepository implements Repository
+final class FakeRepository implements Repository, ObjectIdentifier
 {
-    private $documentation;
+    private $repository;
+
+    public function __construct()
+    {
+        $this->repository = new InMemoryRepository($this);
+    }
 
     public function save(PublishedDocumentation $published)
     {
-        $this->documentation[(string)$published->getDocumentationId()] = $published;
+        $this->repository->save($published);
     }
 
     public function find(DocumentationId $anId)
     {
-        return $this->documentation[(string)$anId];
+        return $this->repository->findById((string)$anId);
     }
 
     public function findForProject($projectName)
     {
         return array_filter(
-            $this->documentation,
+            $this->repository->getAll(),
             function (PublishedDocumentation $documentation) use ($projectName) {
-                return strtolower($projectName) == $documentation->getDocumentationId(
-                )->getProjectName();
+                return strtolower($projectName) == $documentation->getDocumentationId()->getProjectName();
             }
         );
+    }
+
+    public function getIdentity($object)
+    {
+        return (string)$object->getDocumentationId();
     }
 }
