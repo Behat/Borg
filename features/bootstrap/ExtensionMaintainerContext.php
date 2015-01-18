1f466aab9b8249969c08c212ca06cd3effc4e7ed
everzet <ever.zet@gmail.com>
2015-01-18 17:16:56 +0000

    Initialize extension maintainer context suite

---
+++ features/bootstrap/ExtensionMaintainerContext.php
@@ -0,0 +1,17 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class ExtensionMaintainerContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     */
+    public function __construct()
+    {
+    }
+}
