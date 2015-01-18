468550c1c126e31b87dcb17e9cf1aa746020edde
everzet <ever.zet@gmail.com>
2015-01-18 21:09:04 +0000

    Remove useless license block for now

--- src/Application/Infrastructure/Extension/PersistedObjectsRepository.php
+++ src/Application/Infrastructure/Extension/PersistedObjectsRepository.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of the borg.
- * (c) Konstantin Kudryashov <ever.zet@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Behat\Borg\Application\Infrastructure\Extension;
 
 use Behat\Borg\Extension\Extension;
