64cbad43a421b85708b5dc0a3eed67c2332baf97
everzet <ever.zet@gmail.com>
2015-01-18 17:18:00 +0000

    Append snippets for the first extension scenario

--- features/bootstrap/ExtensionMaintainerContext.php
+++ features/bootstrap/ExtensionMaintainerContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 
@@ -13,5 +14,37 @@ class ExtensionMaintainerContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given :arg1 extension was created in :arg2
+     */
+    public function extensionWasCreatedIn($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I release :arg1 version :arg2
+     */
+    public function iReleaseVersion($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then extension catalogue should contain :arg1 extension
+     */
+    public function extensionCatalogueShouldContainExtension($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then :arg1 extension should be in it
+     */
+    public function extensionShouldBeInIt($arg1)
+    {
+        throw new PendingException();
     }
 }
