d931b8378adb3bab6482cd14fcfaeaa2a22e88da
everzet <ever.zet@gmail.com>
2015-03-15 19:28:40 +0000

    Implement extension listing controller

--- src/Application/ExtensionBundle/Controller/ExtensionController.php
+++ src/Application/ExtensionBundle/Controller/ExtensionController.php
@@ -2,10 +2,27 @@
 
 namespace Behat\Borg\Application\ExtensionBundle\Controller;
 
+use Behat\Borg\ExtensionCatalogue;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ExtensionController extends Controller
 {
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function listAction()
+    {
+        return ['extensions' => $this->catalogue()->all()];
+    }
+
+    /**
+     * @return ExtensionCatalogue
+     */
+    private function catalogue()
+    {
+        return $this->get('extension.catalogue');
+    }
 }
