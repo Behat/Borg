4187c86013ce2093e25a0c93859b77c698260044
everzet <ever.zet@gmail.com>
2015-03-16 23:29:31 +0000

    Fix Extension bundle to be inline with master changes

--- src/Application/Extension/DependencyInjection/ExtensionExtension.php
+++ src/Application/Extension/DependencyInjection/ExtensionExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Behat\Borg\Application\ExtensionBundle\DependencyInjection;
+namespace Behat\Borg\Application\Extension\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
