2029a2176e4ce5238eef5971bd3d7f4713f8d6a1
everzet <ever.zet@gmail.com>
2015-03-16 23:56:43 +0000

    Implement extraction of non-composer package

--- spec/Integration/Extension/Composer/ComposerExtractorSpec.php
+++ spec/Integration/Extension/Composer/ComposerExtractorSpec.php
@@ -3,6 +3,7 @@
 namespace spec\Behat\Borg\Integration\Extension\Composer;
 
 use Behat\Borg\Extension\Extractor\Extractor;
+use Behat\Borg\Release\Package;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -11,5 +12,10 @@ class ComposerExtractorSpec extends ObjectBehavior
     function it_is_extension_extractor()
     {
         $this->shouldHaveType(Extractor::class);
+    }
+
+    function it_extracts_nothing_if_provided_package_is_not_a_composer_package(Package $package)
+    {
+        $this->extract($package)->shouldReturn(null);
     }
 }
